10c54184c559bc3769728a9776c090f1e3f7eb31
Wiktor Fogielman <pokepomarchiwizacja@gmail.com>
2024-01-10 13:46:39 +0100

    1 commit

---
+++ app/src/main/java/gerber/apress/com/MainActivity.java
@@ -0,0 +1,15 @@
+package gerber.apress.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
