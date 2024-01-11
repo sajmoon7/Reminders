c089f904eb9e7d342d541e495e06b50d9667a257
Wiktor Fogielman <pokepomarchiwizacja@gmail.com>
2024-01-11 13:42:25 +0100

    Drobne zmiany w bazie danych

--- app/src/main/java/gerber/apress/com/RemindersDbAdapter.java
+++ app/src/main/java/gerber/apress/com/RemindersDbAdapter.java
@@ -1,5 +1,7 @@
 package gerber.apress.com;
 
 public class RemindersDbAdapter {
-
+public static final String COL_ID = "_id";
+public static final String COL_CONTENT = "content";
+public static final String COL_IMPORTANT = "important";
 }
