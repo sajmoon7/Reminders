f1a5083580a86cfd5475f8b0fdf6897e74577d21
Wiktor Fogielman <pokepomarchiwizacja@gmail.com>
2024-01-11 12:55:01 +0100

    3 commit(ListView do kodu Java)

--- app/src/main/java/gerber/apress/com/MainActivity.java
+++ app/src/main/java/gerber/apress/com/MainActivity.java
@@ -3,13 +3,23 @@ package gerber.apress.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.reminders_list_view);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                R.layout.reminders_row,
+                R.id.row_text,
+                new String[]{"pierwszy wiersz", "drugi wiersz", "trzeci wiersz"}
+        );
+        mListView.setAdapter(arrayAdapter);
 
     }
 }
